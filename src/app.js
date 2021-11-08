05f1a68642003d3a95974e6dc7699740c74fb314
Dmytro Tsehelnyk <dtsehelnyk@cybervisiontech.com>
2021-11-09 01:16:31 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const { exit } = require('process');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const SUCCESS = 'Success';
+const NEW_FILE_MESSAGE = 'Enter a name for the new file';
+const NEW_NOTE_MESSAGE = 'New note';
+
+readline.question(`${NEW_FILE_MESSAGE}: `, (fileName) => {
+  readline.question(`${NEW_NOTE_MESSAGE}: `, (note) => {
+    fs.appendFile(`./${fileName}.txt`, `\n${note}`, (error) => {
+      if (error) {
+        console.log(`======= ${error} =======`);
+        exit();
+      }
+
+      console.log(`======= ${SUCCESS} =======`);
+      exit();
+    });
+  });
+});
