10f9000f3f5c8e338b5fe51ca56746bc90f023d8
Oleg Semko <oleg.semko@zazmic.com>
2021-12-12 11:48:41 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+let fileName = '';
+let fileContent = '';
+
+const inter = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+inter.question('enter the file name ', (fileName) => {
+	fileName = fileName;
+	inter.question('enter the file content ', (fileContent) => {
+		fileContent = fileContent;
+		createFile();
+    });
+});
+
+function createFile() {
+	fs.writeFile(`./src/${fileName}.txt`, fileContent, (err, res) => {
+		if (err) {
+			// eslint-disable-next-line no-console
+        	console.log('Error');
+		}
+
+		// eslint-disable-next-line no-console
+      	console.log('Success');
+	});
+
+	inter.close();
+}
