d09a409029db6258fc80e192e788c4f182769747
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-01-18 01:37:49 +0200

    create file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Enter file name: ', (inpNameFile) => {
+  const fileName = inpNameFile;
+
+  terminal.question('Enter the content: ', (inpContent) => {
+    const content = inpContent;
+
+    fs.appendFile(`./src/${fileName}.txt`, `${content}\n`, function(error) {
+      if (error) {
+        throw error;
+      } // если возникла ошибка
+
+      // eslint-disable-next-line no-console
+      console.log('Запись файла завершена. Содержимое файла:');
+
+      const data = fs.readFileSync(`./src/${fileName}.txt`, 'utf8');
+
+      // eslint-disable-next-line no-console
+      console.log(data); // выводим считанные данные
+    });
+
+    terminal.close();
+  });
+});
