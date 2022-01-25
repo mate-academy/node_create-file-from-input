2edde6e782bf8e5c3689c9fb0a06ede0c5e27cbf
Alex Kostomarov <kostomarov.aleks@gmail.com>
2022-01-25 13:08:31 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readlineSync = require('readline-sync');
+const fs = require('fs');
+
+const fileName = readlineSync.question('Enter a file name: ');
+const fileData = readlineSync.question('Enter the file data: ');
+
+fs.writeFile(`./${fileName}.txt`, fileData, (err) => {
+  if (err) {
+    console.log('Error');
+  } else {
+    console.log('Success');
+  }
+});
