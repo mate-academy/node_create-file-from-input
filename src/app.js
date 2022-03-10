67c9c4457f886468b8eaa73639fb61261326d154
Dmytro  Mykoliv <dmytro.mykoliv@indeema.com>
2022-03-10 12:58:33 +0200

    node create file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const rl = require('readline');
+const fs = require('fs');
+
+const terminal = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Enter file name: ', (fileName) => {
+  terminal.question('Add content: ', (content) => {
+    fs.writeFile(`./src/${fileName}.txt`, content, (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+
+        return;
+      }
+      // eslint-disable-next-line no-console
+      console.log('File created');
+    });
+
+    terminal.close();
+  });
+});
