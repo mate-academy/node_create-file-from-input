c73507b0ec91d1dbcffa282c79c41e35ea2522d3
Nastya <anastasia.zasiadovk@ivvysoft.com>
2022-03-10 11:43:01 +0200

    console fix

--- src/fileActions.js
+++ src/fileActions.js
@@ -5,11 +5,13 @@ const fs = require('fs');
 module.exports.createFile = (name, content = '') => {
   fs.writeFile(`./${name}`, content, (err, data) => {
     if (err) {
+      // eslint-disable-next-line no-console
       console.log(err);
 
       return;
     }
 
+    // eslint-disable-next-line no-console
     console.log(`File { ${name} } created!`);
   });
 };
