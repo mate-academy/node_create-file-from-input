764e7f0a9bac667ef5c58f484ac2aaac242dbc36
dant0n <dmibuc@gmail.com>
2022-04-02 14:55:28 +0300

    Finish hw.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface(process.stdin, process.stdout);
+
+terminal.question('Enter file name: ', (name) => {
+  terminal.question('Enter file content: ', (content) => {
+    writeFile(name, content);
+    terminal.close();
+  });
+});
+
+function writeFile(name, data) {
+  fs.writeFile(name, data, (error) => {
+    if (error) {
+      console.log('Error');
+
+      return;
+    }
+    console.log('Success');
+  });
+}
