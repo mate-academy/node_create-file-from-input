092005d73f0aec7d864e255b57ab88329aaf4e32
Taras Pyda <tapac1991@icloud.com>
2022-05-22 10:40:31 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Please enter a file name ', (name) => {
+  terminal.question('Please enter a file description ', (description) => {
+    fs.writeFile(`./${name}.txt`, description, (err) => {
+      if (err) {
+        console.log(('Error'));
+
+        return;
+      };
+
+      console.log('Success');
+    });
+
+    terminal.close();
+  });
+});
