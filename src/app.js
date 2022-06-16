021a3d6bf42cafb15fa4ff209c57020f20cb84ff
E-Sharpilo <evgeniy.sharpilo@gmail.com>
2022-06-16 10:21:54 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+const myConsole = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+myConsole.question('Enter please file name ', (fileName) => {
+  myConsole.question('Enter please content ', (fileContent) => {
+    fs.writeFile(fileName, fileContent, (error) => {
+      myConsole.output.write(error ? 'Error' : 'Success');
+      myConsole.close();
+    });
+  });
+});
