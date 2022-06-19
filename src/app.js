2f865bd29f65debdebfe5f3fbade204fb959f08a
Dima Permiakov <permiakov.d.s@gmail.com>
2022-06-19 13:45:39 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const readline = require('readline');
+const { created } = require('./created');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Enter name: ', (nameFile) => {
+  terminal.question('Enter content: ', (content) => {
+    created(nameFile, content);
+
+    terminal.close();
+  });
+});
