dcc8acfeab784788f42449249a1dbbcf3d5fa820
yatochkaaa <volodymyr.taranin@gmail.com>
2022-08-12 17:18:34 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
 'use strict';
+import readline from 'readline';
+import * as fs from 'fs';
+
+// - When the user completes interaction, your app must create a file and output `Success` or `Error` depending on the result of creation.
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const fileNameCallback = (userTitleInput) => {
+  const title = userTitleInput;
+
+  if (title) {
+    rl.question(`enter the content: `, (userContentInput) => {
+      const content = userContentInput;
+
+      fs.writeFile(`./src/${title}`, content, (err) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+      rl.close();
+    });
+  } else {
+    console.log('write correct title');
+    rl.question(`enter the file name: `, fileNameCallback);
+  }
+}
+
+rl.question(`enter the file name: `, fileNameCallback);
