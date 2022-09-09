ece922cbad3ea780384aa964142134f171403f4a
Roman Kovalchuk <roma220702@gmail.com>
2022-09-09 19:45:28 +0300

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('Введіть назву файлу\n', (name) => {
+  fs.writeFile(`./src/${name}.txt`, '', () => {
+  });
+
+  terminal.question('Введіть контент файлу\n', (content) => {
+    fs.appendFile(`./src/${name}.txt`, content, () => {
+    });
+    terminal.close();
+  });
+});
