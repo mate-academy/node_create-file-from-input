aaa8e6981e28c0c600dae6fbe8b3736418e4faad
Sasha Yatsykovych <olexandr.y@mate.academy>
2022-09-18 20:05:02 +0300

    add solution with promises

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const readline = require('readline/promises').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const fs = require('fs');
+
+async function main() {
+  try {
+    const fileName = await readline.question('Enter file name: ');
+    const fileContent = await readline.question('Enter file content: ');
+
+    readline.close();
+
+    fs.writeFile(fileName, fileContent, (error) => {
+      if (error) {
+        console.error('Error: ', error);
+      }
+
+      console.log('Success');
+    });
+  } catch (error) {
+    console.error('Error: ', error);
+  }
+}
+
+main();
