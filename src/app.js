722c5bcc1624bd22f686323569ac00a85566e18d
Pavlo <Pmoroz2004@gmail.com>
2022-09-20 14:39:20 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const baseURL = './src/newFolders';
+
+terminal.question('Write name of your file > ', (name) => {
+  const correctName = name.trim();
+
+  if (correctName) {
+    terminal.question('Write content of your file > ', (content) => {
+      fs.writeFile(`${baseURL}/${correctName}.txt`, content, (err) => {
+        if (err) {
+          console.log('Error');
+        }
+
+        terminal.close();
+      });
+      console.log('Success');
+    });
+  } else {
+    console.log('Please enter correct name');
+    terminal.close();
+  }
+});
