65663185c9418ecfbe86a6c2c5ec2efb2704dc62
Aleksandr Yuschenko <105875778+AleksandrYuschenko@users.noreply.github.com>
2022-09-26 18:07:10 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+
+const { terminal } = require('./terminal');
+
+let fileName = '';
+let fileContent = '';
+
+terminal.question('Enter the file name\n', (resTitle) => {
+  fileName = resTitle;
+
+  if (fileName === '') {
+    console.log('Error: enter title.');
+    terminal.close();
+
+    return;
+  }
+
+  terminal.question('Enter the content\n', (resContent) => {
+    fileContent = resContent;
+
+    if (fileName !== '' && fileContent !== '') {
+      fs.writeFile(`./sr/${fileName}`,
+        fileContent, er => er
+          ? console.log('Error', er.code)
+          : console.log('Success'));
+      terminal.close();
+    }
+  });
+});
---
+++ src/terminal.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-console */
+'use strict';
+
+const readline = require('readline');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+exports.terminal = terminal;
