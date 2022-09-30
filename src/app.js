ff5423afff2c96f7d8dcaf69cf24c64d71abfefe
Oleksii Budnik <budnikoleksii@gmail.com>
2022-09-30 13:10:04 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 'use strict';
+/* eslint-disable no-console */
+
+const readline = require('readline');
+const fs = require('fs/promises');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const createFile = () => {
+  terminal.question('Please, enter file name: ', (fileName) => {
+    if (!fileName.trim()) {
+      console.log('File name should at least one letter');
+
+      return createFile();
+    }
+
+    terminal.question('Please, enter file content: ', (fileContent) => {
+      fs.writeFile(fileName, fileContent)
+        .then(() => console.log('Success'))
+        .catch(() => console.log('Error'))
+        .finally(() => terminal.close());
+    });
+  });
+};
+
+createFile();
