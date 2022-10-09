d0f3279cd42d64eb5a094a706a6c109a3f3fdbff
Kate <krasnova2912@gmail.com>
2022-10-09 21:53:54 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,39 @@
+/* eslint-disable no-console */
 'use strict';
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const main = async() => {
+  const fileData = await inquirer.prompt([
+    {
+      name: 'fileName',
+      type: 'input',
+      message: 'write file name',
+      validate: (str) => str.length > 0,
+    },
+    {
+      name: 'contant',
+      type: 'input',
+      message: 'wrint file contant',
+    },
+  ]);
+
+  console.log(fileData);
+
+  fs.writeFile(
+    `./src/${fileData.fileName}.txt`,
+    fileData.contant,
+    (err, data) => {
+      if (err) {
+        console.log('Error');
+
+        return;
+      }
+
+      console.log('Success');
+    }
+  );
+};
+
+main();
