5ca3860b1d519f3b6565acfcddde89bcc2cea6b1
Kate-Khon <katerynakhon@gmail.com>
2022-10-12 23:22:24 +0300

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,61 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs/promises');
+
+const start = () => {
+  const terminal = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  let fileName = '';
+  let fileContent = '';
+
+  const createAFile = () => {
+    fs.writeFile(`./${fileName}`, fileContent);
+
+    fs.readFile(`./${fileName}`, 'utf8')
+      .then(() => {
+        console.log('Success');
+      })
+      .catch(() => {
+        console.log('Error');
+      })
+      .finally(() => {
+        terminal.close();
+      })
+  }
+
+  const getFileContent = () => {
+    terminal.question('Enter a file content ', (content) => {
+      if (!content.trim()) {
+        getFileContent();
+
+        return;
+      }
+
+      fileContent = content;
+
+      createAFile();
+    });
+  }
+
+  const getFileName = () => {
+    terminal.question('Enter a file name ', (name) => {
+      if (!name.trim()) {
+        getFileName();
+
+        return;
+      }
+
+      fileName = name;
+
+      getFileContent();
+    });
+  }
+
+  getFileName();
+}
+
+start();
