1716302842a9470cdb47f13d98f94d490fa74522
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-10-12 15:06:14 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function createFile() {
+  terminal.question('Enter the name\n', (fileName) => {
+    if (!fileName) {
+      createFile();
+    } else {
+      terminal.question('\nEnter content\n', (content) => {
+        fs.writeFile(`./src/${fileName}.txt`, content, (error) => {
+          if (!error) {
+            terminal.write('Success');
+            terminal.close();
+          } else {
+            terminal.write('Error');
+            terminal.close();
+          }
+        });
+      });
+    }
+  });
+}
+
+createFile();
