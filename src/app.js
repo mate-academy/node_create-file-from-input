ffcb368d09ffb79af8a3062a12566c2d205b17e5
Denys Korotkoruchko <denenergii@gmail.com>
2022-10-12 23:29:36 +0300

    ready

--- src/app.js
+++ src/app.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const redline = require('readline');
+const fs = require('fs');
+
+let name = '';
+let text = '';
+
+const terminal = redline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+(function enterFileName() {
+  terminal.question('enter file name: ', (input) => {
+    name = input;
+    enterText();
+  });
+}());
+
+function enterText() {
+  terminal.question('enter text file: ', (input) => {
+    text = input;
+    result();
+    terminal.close();
+  });
+}
+
+function result() {
+  if (name !== '') {
+    fs.writeFile(`./${name}.txt`, text, (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log('ERROR');
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('SUCCES');
+      }
+    });
+  }
+};
