cdb58a638c2119a99a00df77907200e8a8fba6a6
Anton Kulchytskyi <antek1809@gmail.com>
2022-10-13 02:30:26 +0200

    solution node create file from input

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const fs = require('fs');
+const { terminal } = require('./terminal');
+
+const question = (questions) => {
+  return new Promise((resolve) => {
+    terminal.question(questions, (answer) => {
+      resolve(answer);
+    });
+  });
+};
+
+const main = async() => {
+  let fileName = await question('Please enter a file name: ');
+
+  if (!fileName) {
+    terminal.write('You must enter a file name \n');
+    fileName = await question('Please enter a file name: ');
+  }
+
+  const fileContext = await question('Please enter a file content: ');
+
+  fs.writeFile(`./src/${fileName}`, fileContext, (error) => {
+    if (error) {
+      terminal.write('Error');
+      terminal.close();
+
+      return;
+    }
+
+    terminal.write('Success');
+    terminal.close();
+  });
+};
+
+main();
