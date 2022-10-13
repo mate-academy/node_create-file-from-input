e700ef80a2c3836604339c8a0522e9d2bcafeb36
Maks Sobko <tiserettt@gmail.com>
2022-10-13 13:41:02 +0300

    implemented function to create files

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const { writeFile } = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const processFile = () => {
+  terminal.question('Enter file name: ', fileName => {
+    if (!fileName) {
+      processFile();
+    }
+
+    terminal.question('Enter file content: ', fileContent => {
+      writeFile(fileName, fileContent, 'utf-8', err => {
+        if (err) {
+          console.log('Error');
+        } else {
+          console.log('Success ');
+        }
+
+        process.exit();
+      });
+    });
+  });
+};
+
+processFile();
