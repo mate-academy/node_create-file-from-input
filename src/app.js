3e7676bf26f2a523eb576b88238d0bdc17e0bff4
Anton Liada <liadaantom@gmail.com>
2022-10-14 20:17:59 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const { writeFile } = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const processFile = () => {
+  terminal.question('Please, enter the file name ', (fileName) => {
+    if (!fileName.trim()) {
+      processFile();
+    }
+
+    terminal.question('Please, enter the content ', (content) => {
+      writeFile(fileName, content, 'utf8', (err) => {
+        if (err) {
+          console.log('Error');
+        }
+      });
+
+      console.log('Success');
+      terminal.close();
+    });
+  });
+};
+
+processFile();
