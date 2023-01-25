37da7c9603228f8c3c315d5233dcefb2d607508d
M.Ali Yücesoylu <myucesoylu@gmail.com>
2023-01-25 17:35:44 +0300

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const createFile = async() => {
+  terminal.question('Enter the name of the file\n', (fileName) => {
+    if (!fileName) {
+      createFile();
+
+      return;
+    }
+
+    terminal.question('\nEnter contents of the file\n', (fileContent) => {
+      fs.writeFile(`./src/${fileName}.txt`, fileContent, (error) => {
+        const message = error ? 'Error' : 'Success';
+
+        terminal.write(message);
+        terminal.close();
+      })
+    })
+  })
+}
+
+createFile();
