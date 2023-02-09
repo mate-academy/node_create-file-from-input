076cd68c31dda694aa8c4e55f808ab25da4b02a6
Bradley Matjie <bradleysaint45@gmail.com>
2023-02-09 11:31:26 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function createFile() {
+  rl.question('Enter file name: ', (fileName) => {
+    if (!fileName) {
+      global.console.log('Enter correct file name: ');
+      createFile();
+    }
+
+    rl.question('Enter your content: ', (content) => {
+      rl.close();
+
+      fs.writeFile(`./src/${fileName}.txt`, content, (error) => {
+        if (error) {
+          global.console.log('Error');
+        } else {
+          global.console.log('Success');
+        }
+      });
+    });
+  });
+}
+
+createFile();
