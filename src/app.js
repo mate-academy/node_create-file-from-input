83452b27cf59cd889eb149d15358fafe36065bfd
ysrckr <dyt.yasarcakir@gmail.com>
2023-02-12 00:33:40 +0300

    completed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const { askFileName, askForContent } = require('./io');
+const createFile = require('./file');
+
+const fileName = askFileName();
+const content = askForContent();
+
+createFile(fileName, content);
