5e7d97a46a336009f585d06ddf9cc34bb82f9e36
Lefteris Loizides <loizides.lef@gmail.com>
2023-02-14 10:08:30 +0200

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
-'use strict';
+import rl from 'readline';
+import fs from 'fs';
+
+const terminal = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const prompt = () => {
+  const createFile = (fileName) => {
+    const path = `./${fileName}`;
+
+    const checkFile = () => {
+      // eslint-disable-next-line no-console
+      console.log(fs.existsSync(path)
+        ? 'Success'
+        : 'Error'
+      );
+      terminal.close();
+    };
+
+    const createContent = (content) => {
+      fs.writeFile(path, content, checkFile);
+    };
+
+    if (!fileName) {
+      prompt();
+    }
+
+    terminal.question('Enter file content: ', createContent);
+  };
+
+  terminal.question('Enter file name: ', createFile);
+};
+
+prompt();
