d33c7dd267fbfd7031f5dc561b05118265253cc4
maxim2310 <m23a09x96@gmail.com>
2023-02-19 20:50:23 +0200

    file can be created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function createFileByName() {
+
+  terminal.question('Plz enter file name... ', (name) => {
+    if (!name.length) {
+      console.log('enter a valid name');
+      createFileByName();
+    }
+
+    terminal.question('Enter the content... ', (content) => {
+      fs.writeFile(`${name}.txt`, content, 'utf8', (err) => {
+        if (err) {
+          console.log('Error');
+        } else {
+          console.log('Success');
+        }
+        terminal.close();
+      });
+    });
+  });
+}
+
+createFileByName();
