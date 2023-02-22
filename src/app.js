db53ef99c1e4a0b1bedc6eeba85f5fcc92fdea80
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2023-02-22 13:26:04 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,35 @@
+/* eslint-disable no-console */
 'use strict';
+
+const readline = require('readline');
+const fs = require('fs');
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.setPrompt('Please provide a file name  ');
+terminal.prompt();
+
+terminal.on('line', (filename) => {
+  if (!filename) {
+    console.log('Error: File name cannot be empty');
+    terminal.prompt();
+  } else {
+    terminal.removeAllListeners('line');
+    terminal.setPrompt('Please provide a content for the file  ');
+    terminal.prompt();
+
+    terminal.on('line', (content) => {
+      fs.writeFile(filename, content, (error) => {
+        if (error) {
+          console.log('Error');
+        } else {
+          console.log('Success');
+        }
+        terminal.close();
+      });
+    });
+  }
+});
