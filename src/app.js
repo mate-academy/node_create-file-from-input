71447be7897984f2d5358a4fdaf1ec80ba1b6300
Levente Krotos <krotos.levi@gmail.com>
2023-03-23 15:36:06 +0100

    solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+import fs from 'fs'
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const createFile = () => {
+  rl.question('file name: ',(fileName) => {
+    rl.question('file content: ',(fileContent) => {
+      fs.writeFile(`./${fileName}.txt`, fileContent, (err) => {
+        if(err) {
+          console.log(`Error: ${err}`)
+        } else {
+          console.log(`file created with a name of '${fileName}'`)
+        }
+        rl.close();
+      });
+    });
+  });
+}
+
+
+createFile();
