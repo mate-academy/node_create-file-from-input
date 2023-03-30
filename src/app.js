637765998c536cf37aacd5e3617ec22cebe85366
Nicusor Iorga <ioghigithublab@gmail.com>
2023-03-31 00:21:40 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-'use strict';
+/* eslint-disable no-console */
+import fs from 'fs';
+import readline from 'readline';
+
+let fileName = '';
+let fileContent = '';
+
+const ask = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+ask.question('Filename ?', (name) => {
+  fileName = name;
+});
+
+ask.question('Filecontent ?', (content) => {
+  fileContent = content;
+});
+
+fs.appendFile(`./${fileName}`, fileContent, (error, data) => {
+  if (error) {
+    console.log(error);
+
+    return '';
+  }
+
+  console.log(data.toString());
+});
