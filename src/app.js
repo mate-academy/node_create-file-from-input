6bd3d57b40a213207c7497c005920f3dd3853346
Jānis Žurilo <zurilojohn@gmail.com>
2023-05-01 18:44:20 +0300

    Finished

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+import fs from 'fs';
+import readLine from 'readline';
+
+const terminal = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+terminal.question('provide a file name:', (fileName) => {
+  fs.writeFile(`src/${fileName}.txt`, '', () => {});
+
+  terminal.question('Provide the content of the file: ', (content) => {
+    fs.appendFile(`src/${fileName}.txt`, `${content}`, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('The file has been saved!');
+      }
+    });
+    terminal.close();
+  });
+});
