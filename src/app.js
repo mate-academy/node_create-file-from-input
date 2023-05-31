d1eccfb7e461fd71cad68a114cb90aa794d04cb1
Metin BICAKSIZ <metinbicaksiz@mail.com>
2023-05-31 18:49:52 +0300

    first attempt

--- src/app.js
+++ src/app.js
@@ -1,1 +1,34 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function createFile() {
+  rl.question('Enter the file name: ', (fileName) => {
+    if (fileName.trim() === '') {
+      console.log('Error: File name cannot be empty.');
+      rl.close();
+
+      return;
+    }
+
+    rl.question('Enter the content: ', (content) => {
+      fs.writeFile(fileName, content, (err) => {
+        if (err) {
+          console.log('Error:', err.message);
+        } else {
+          console.log('Success: File created successfully.');
+        }
+        rl.close();
+      });
+    });
+  });
+}
+
+createFile();
