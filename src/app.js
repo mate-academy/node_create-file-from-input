13e15c12d128c70e25e1a753e361d956f42ce14f
Oleksandr Kapustei <angelion541@gmail.com>
2023-08-24 09:53:07 +0300

    finish task

--- src/app.js
+++ src/app.js
@@ -1,1 +1,48 @@
-'use strict';
+"use strict";
+
+import readline from "readline";
+import fs from "fs/promises";
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let userFilename = "";
+
+function controller(flag, value) {
+  if (flag === "filename") {
+    getFileData(value);
+  } else if (flag === "content") {
+    fileCreation(value);
+  }
+}
+
+function getFileData(value) {
+  if (value === undefined || value.length === 0) {
+    terminal.question("Please enter name of new file: \n", (value) =>
+      controller("filename", value)
+    );
+  }
+
+  userFilename = value;
+
+  terminal.question("Please enter content of the file: \n", (value) =>
+    controller("content", value)
+  );
+}
+
+function fileCreation(value) {
+  fs.writeFile(`src/${userFilename}.txt`, value)
+    .then((res) => {
+      console.log("Success");
+    })
+    .catch((err) => {
+      console.log("Error");
+    })
+    .finally(() => {
+      terminal.close();
+    });
+}
+
+controller("filename");
